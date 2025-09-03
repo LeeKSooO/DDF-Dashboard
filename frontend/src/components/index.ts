588aff189cf93e9c07f5cc7a2690ef03a38c181c
이경수 <kyoungsoo0301@gmail.com>
2025-09-03 14:22:26 +0900

    delete: frontend

--- frontend/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-// 모든 컴포넌트를 한 곳에서 export
-export { Dashboard } from './Dashboard/Dashboard';
-export { DRTMap } from './Map/DRTMap';
-export { HeatmapLayer } from './Map/HeatmapLayer';
-export { StopMarker } from './Map/StopMarker';
-export { TimeSelector } from './TimeSelector/TimeSelector';
-export { StatsPanel } from './StatsPanel/StatsPanel';
